81a4cdd3cd6e9a5522f6f25af2c0ba22d060b394
CoreyC <3529173-CoreyC@users.noreply.gitlab.com>
2021-05-19 12:51:57 -0600

    Change to fixed-width sidebar layout.

--- data_ui.R
+++ data_ui.R
@@ -24,6 +24,33 @@ id_ctrl <- conditionalPanel(
   # radioButtons("id_radio", "Sample ID Column", choices = c(1))
   selectInput("d_id_col", "ID Column Name", choices = NULL))
 
+data_sidebar <- div(
+  style = "min-width: 240px; max-width: 240px;",
+  wellPanel(
+    conditionalPanel(
+      condition = "input.d_tabs == 'Phenotype'",
+      upload_ctrl,
+      file_ctrl,
+      excel_ctrl,
+      txt_ctrl),
+    conditionalPanel(condition = "input.d_tabs == 'Phenotype' || input.d_tabs == 'Cleanup'", id_ctrl),
+    conditionalPanel(
+      condition = "input.d_tabs == 'Cleanup'",
+      checkboxGroupInput("clean_cols", "Convert Columns to Number", choices = NULL),
+      p("Select Columns to remove all non-digits and convert to number.")
+    )))
+
+data_tabs <- div(
+  style="display:inline-block; min-width: 400px; padding-left:25px; padding-top:10px",
+  tabsetPanel(
+    id = "d_tabs",
+    type = "pills",
+    # tabPanel( "Phenotype", uiOutput('uploadUI') ),
+    tabPanel("Phenotype", tableOutput("pheno")),
+    tabPanel("Cleanup", tableOutput("cleaned_pheno")),
+    tabPanel("Filter", tableOutput("filtered_pheno"))
+  ))
+
 data_ui <- tabPanel( titlePanel("Load Data"),
   splitLayout(
     cellWidths = c("60%", "20%", "20%"),
@@ -39,30 +66,5 @@ data_ui <- tabPanel( titlePanel("Load Data"),
     tags$style(type = 'text/css', "#disp { margin-top: 15px; }"),
     tags$style(type = 'text/css', "#d_downloadManifest { margin-top: 20px; }")
   ),
-  sidebarLayout(
-    sidebarPanel(
-      conditionalPanel(
-        condition = "input.d_tabs == 'Phenotype'",
-        upload_ctrl,
-        file_ctrl,
-        excel_ctrl,
-        txt_ctrl
-      ),
-      conditionalPanel(condition = "input.d_tabs == 'Phenotype' || input.d_tabs == 'Cleanup'", id_ctrl),
-      conditionalPanel(
-        condition = "input.d_tabs == 'Cleanup'",
-        checkboxGroupInput("clean_cols", "Convert Columns to Number", choices = NULL),
-        p("Select Columns to remove all non-digits and convert to number.")
-      )),
-    mainPanel(
-      tabsetPanel(
-        id = "d_tabs",
-        type = "pills",
-        # tabPanel( "Phenotype", uiOutput('uploadUI') ),
-        tabPanel("Phenotype", tableOutput("pheno")),
-        tabPanel("Cleanup", tableOutput("cleaned_pheno")),
-        tabPanel("Filter", tableOutput("filtered_pheno"))
-      )
-    )
-  )
+  flowLayout( data_sidebar, data_tabs )
 )
