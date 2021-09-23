daa28524d6c1850d752b3b30c52a0097a1cdf558
CoreyC <3529173-CoreyC@users.noreply.gitlab.com>
2021-09-23 14:24:58 -0600

    Re-enable QC tab.

--- app.R
+++ app.R
@@ -19,26 +19,21 @@ library(readxl)
 
 
 source("data_ui.R", local = TRUE)
-#source("qc_ui.R", local = TRUE)
+source("qc_ui.R", local = TRUE)
 source("manifest_ui.R", local = TRUE)
 
 ui <- shinyUI(fluidPage(
   titlePanel("CARGO Manifest Generator", windowTitle = "CARGO Manifest Generator"),
-  # tabsetPanel( id = "top_tabs", data_ui, qc_ui, manifest_ui )
-  tabsetPanel( id = "top_tabs", data_ui, manifest_ui )
-  
-  # tabsetPanel( id = "top_tabs", qc_ui, manifest_ui )
-  # tabsetPanel( id = "top_tabs", data_ui, manifest_ui )
-  # tabsetPanel( id = "top_tabs", data_ui, qc_ui )
+  tabsetPanel( id = "top_tabs", data_ui, qc_ui, manifest_ui )
 ))
 
 source("data_server.R", local = TRUE)
-#source("qc_server.R", local = TRUE)
+source("qc_server.R", local = TRUE)
 source("manifest_server.R", local = TRUE)
 
 server <- shinyServer(function(input, output, session) {
   data_server(input, output, session)
-  #qc_server(input, output, session)
+  qc_server(input, output, session)
   manifest_server(input, output, session)
 })
 
